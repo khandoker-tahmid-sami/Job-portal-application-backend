e16db4554f83a98afcc5d8f23822aa20d90cd555
khandoker Tahmid Sami <prinon96@gmail.com>
2026-05-10 21:48:16 +0200

    changing to mongodb database part 1

--- config/database.js
+++ config/database.js
@@ -1,10 +1,19 @@
-const { Sequelize } = require('sequelize');
-const path = require('path');
+// const { Sequelize } = require('sequelize');
+// const path = require('path');
 
-const sequelize = new Sequelize({
-    dialect: 'sqlite',
-    storage: process.env.DB_STORAGE || './database.sqlite',
-    logging: false, // Set to console.log to see SQL queries
-});
+// const sequelize = new Sequelize({
+//     dialect: 'sqlite',
+//     storage: process.env.DB_STORAGE || './database.sqlite',
+//     logging: false, // Set to console.log to see SQL queries
+// });
 
-module.exports = sequelize;
+// module.exports = sequelize;
+
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  const conn = await mongoose.connect(process.env.MONGO_URI);
+  console.log(`Database Connection Successfull: ${conn.connection.host}`);
+};
+
+module.exports = connectDB;
